3bc7648d51259b7e7ebdb224206d9193eaf80608
nikos_liakas <liakasn@gmail.com>
2024-10-17 20:12:30 +0100

    connection to MongoDB and environment variables

--- week2_python-with-mongo/mongodb_demo.py
+++ week2_python-with-mongo/mongodb_demo.py
@@ -1,19 +1,25 @@
+from dotenv import load_dotenv
 from pprint import pprint
 import pymongo
+import os
 from pymongo import MongoClient
+
+
 
 """
 All of the below are selections from:
 https://github.com/warestack/cc/blob/master/Class-2/Lab2.3-MongoDB-tutorial-with-Python.md
 """
 
-server = "mongodb+srv://student:1234@cluster.qz1mk.mongodb.net/?retryWrites=true&w=majority&appName=cluster"
 
-conn = MongoClient(server)
 
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    server = f"mongodb+srv://{os.getenv('MONGO_USER_ID')}:{os.getenv('MONGO_PWD')}@cluster.qz1mk.mongodb.net/?retryWrites=true&w=majority&appName=cluster"
+
+    conn = MongoClient(server)
     # using the connection to the database we can retrieve the collection cluster
     col_films = conn.MiniFilms.films
 
